adc6a745d9ec4d8080955e5583915ce69c201a7a
AHdr0Hchik <xomenko.andrey2201@gmail.com>
2024-09-25 18:28:32 +0300

    456

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from "../Button/Button";
 import './ProductItem.css';
+import imagePaths from '../../hooks/imgRouter';
 
 const ProductItem = ({product, className, onAdd}) => {
 
@@ -9,16 +10,36 @@ const ProductItem = ({product, className, onAdd}) => {
     }
 
     return (
-        <div className={'eventCard ' + className}>
-            <img className={'img'} src={'../../public/img/eventImg'+product.id} alt="" />
-            <div className={'title'}>{product.title}</div>
-            <div className={'description'}>{product.description}</div>
-            <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+        <div className="upgrade-special" style={{ transform: 'scale(1) translateZ(0px)', opacity: 1 }}>
+            <div className="upgrade-special-inner int_year_strategy">
+                <div className="upgrade-special-top">
+                    {/* Можно добавить содержимое или удалить, если не нужно */}
+                </div>
+                <div className="upgrade-special-image int_year_strategy">
+                    <div className="upgrade-image">
+                        <picture className="is-rounded">
+                            <source 
+                                srcSet="https://i.imgur.com/be49oBD.png" 
+                                type="image/png" 
+                                alt="int_year_strategy" 
+                            />
+                            <img 
+                                className="img-responsive" 
+                                src="https://i.imgur.com/be49oBD.png" 
+                                alt="int_year_strategy" 
+                            />
+                        </picture>
+                    </div>
+                </div>
+                <div className="upgrade-special-title">Year strategy</div>
+                <div className="upgrade-special-spacer"></div>
+                <div className="upgrade-special-text">Planning time</div>
+                <div className="upgrade-special-bottom">
+                    <div className="upgrade-special-detail is-center">
+                        <p className="is-text-center">Вы владеете этой картой</p>
+                    </div>
+                </div>
             </div>
-            <Button className={'add-btn'} onClick={onAddHandler}>
-                Добавить в корзину
-            </Button>
         </div>
     );
 };
---
+++ src/hooks/imgRouter.js
@@ -0,0 +1,6 @@
+const imagePaths = {
+    eventImg1: '../img/eventImg1.jpg'
+    // добавьте остальные изображения
+};
+
+export default imagePaths;
